151da3244b4064f81af8cd11a9a2a54d2ec1065d
MatthewSantoro <matthewsantoro@protonmail.com>
2020-01-28 13:28:40 +0500

    add models in admin

--- mysite/stock/admin.py
+++ mysite/stock/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Products, Ranks, Statuses
 
-# Register your models here.
+admin.site.register(Products)
+admin.site.register(Ranks)
+admin.site.register(Statuses)
