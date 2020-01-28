894d994d56dd42ac6722831032374e9b070cfa4c
MatthewSantoro <matthewsantoro@protonmail.com>
2020-01-28 13:10:22 +0500

    create models and make migrations

--- mysite/stock/models.py
+++ mysite/stock/models.py
@@ -5,18 +5,21 @@ from django.utils import timezone
 # Create your models here.
 
 
+class Statuses(models.Model):
+    title = models.CharField(max_length=100)
+
+class Ranks(models.Model):
+    title = models.CharField(max_length=100)
+
 class Products(models.Model):
     title =models.CharField(max_length=200)
     count = models.IntegerField()
     desc = models.TextField()
     rank = models.ForeignKey(Ranks, on_delete=models.CASCADE)
     status = models.ForeignKey(Statuses, on_delete= models.CASCADE)
+    start_date = models.DateTimeField(default=timezone.now())
+    end_date = models.DateTimeField()
 
-
-class Statuses(models.model):
-    title = models.CharField(max_length=100)
-
-class Ranks(models.Model):
-    title = models.CharField(max_length=100)
-    
-
+    def __str__(self):
+        return self.title
+  
